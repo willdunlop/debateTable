e061ddbd1aeb54fd883c1dd179f1573834524b1e
willdunlop <william.dunlop.1993@gmail.com>
2016-12-06 13:09:40 +1100

    comment buttons now all work, need to fix up the form communication for 'send' button

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 var url = process.env.MLAB_DT_URI;
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost/debateTable');
-mongoose.connect(url);
+mongoose.connect('mongodb://localhost/debateTable');
+// mongoose.connect(url);
